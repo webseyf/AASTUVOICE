2abbb04ea26a8bba217f3e64329026cddbaeb474
seyfadin <seyfadinabdela42@gmail.com>
2024-12-02 17:19:13 +0300

    initialize

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBGCdycrKLfgLkdFJxc3s6MveN9O9WEaM",
+  authDomain: "ethioblogs-93bb4.firebaseapp.com",
+  projectId: "ethioblogs-93bb4",
+  storageBucket: "ethioblogs-93bb4.firebasestorage.app",
+  messagingSenderId: "92397453630",
+  appId: "1:92397453630:web:9bfaef989186cdc90cac6f",
+  measurementId: "G-SGMKLW6TLC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
